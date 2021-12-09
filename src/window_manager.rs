548f9aa3bdd731775a0a7d4b408afdb7d075889d
Pierre Viseu Chevalier <pierrechevalier83@gmail.com>
2021-12-09 22:17:57 +0000

    Be more resilient to timing issues
    If a workspace is created or modified since we created our name mapping,
    the current code would crash but the new code no-ops and lives to see
    another day.

--- src/window_manager.rs
+++ src/window_manager.rs
@@ -245,7 +245,7 @@ impl WindowManager {
                     connection
                         .run_command(&format!(
                             "rename workspace \"{}\" to \"{}\"",
-                            &workspace.name, &new_names[&workspace.name]
+                            &workspace.name, &new_names.get(&workspace.name).unwrap_or(&workspace.name)
                         ))
                         .unwrap();
                 }),
@@ -258,7 +258,7 @@ impl WindowManager {
                         connection
                             .run_command(&format!(
                                 "rename workspace \"{}\" to \"{}\"",
-                                &workspace.name, &new_names[&workspace.name]
+                                &workspace.name, &new_names.get(&workspace.name).unwrap_or(&workspace.name)
                             ))
                             .unwrap();
                     })
